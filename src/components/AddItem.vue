1061159618a5dc9a9b4c2ca4908ba781d6dbc44f
JoshuaTakahashi <ruffy.joshua@gmail.com>
2020-03-17 08:41:43 +0800

    Added firebase

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -1,70 +1,66 @@
 <template>
-  <div id=app>
+  <div id="app">
     <h1>Add Item Page</h1>
     <form id="fm1">
-        <label>Item Name</label>
-        <input type="text" v-model.lazy="item.name" required/>
-        <label>Item Category</label>
-        <input type="text" v-model.lazy="item.category"/>
-        <button v-on:click.prevent="addItem">Add Item</button>
-        
+      <label>Item Name</label>
+      <input type="text" v-model.lazy="item.name" required />
+      <label>Item Category</label>
+      <input type="text" v-model.lazy="item.category" />
+      <button v-on:click.prevent="addItem">Add Item</button>
     </form>
   </div>
 </template>
 
 <script>
-
-import database from '../firebase.js'
+import database from "../firebase.js";
 export default {
-
-  data(){
-    return{
-        msg:"Add Item",
-        item:{
-          name:'',
-          category:''
-        },
-        
-        
-        }
+  data() {
+    return {
+      msg: "Add Item",
+      item: {
+        name: "",
+        category: ""
+      }
+    };
   },
-  methods:{
-    addItem:  function () {
-          //Save item to database
-          database.collection('items').doc().set(this.item);
-          this.item.name="";
-          this.item.category="";
-          alert("I am in the DB .... :-) Item saved successfully")
-          
-        }
+  methods: {
+    addItem: function() {
+      //Save item to database
+      database
+        .collection("itemsList")
+        .doc()
+        .set(this.item);
+      this.item.name = "";
+      this.item.category = "";
+      alert("I am in the DB .... :-) Item saved successfully");
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#app *{
-    box-sizing: border-box;
+#app * {
+  box-sizing: border-box;
 }
-#app{
-    margin: 20px auto;
-    max-width: 500px;
+#app {
+  margin: 20px auto;
+  max-width: 500px;
 }
 
-p{
-    align-content: center;
-    color:ivory;
+p {
+  align-content: center;
+  color: ivory;
 }
-label{
-    display: inline-block;
-    margin: 20px 0 10px;
-    width:50%;
-    align-content:left;
-
+label {
+  display: inline-block;
+  margin: 20px 0 10px;
+  width: 50%;
+  align-content: left;
 }
-input[type="text"]{
-    display: inline-block;
-    padding: 8px;
-    width:50%;
+input[type="text"] {
+  display: inline-block;
+  padding: 8px;
+  width: 50%;
 }
 </style>
